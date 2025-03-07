e477b0ba18f67295f96e853e756eebd5b0b91db5
harsha <harshaa.bsc23@rvu.edu.in>
2025-03-07 21:34:42 +0530

    linear search with time complexity

--- linear_search.py
+++ linear_search.py
@@ -1,9 +1,12 @@
+import time
 n=int(input("Enter the number of elements in array: "))
 print("Enter sorted array elements one by one: ")
 arr=[int(input()) for _ in range(n)]
 target=int(input("Enter element to be found: "))
 
 found=False
+
+start_time=time.time()
 
 for i in range(n):
     if arr[i]==target:
@@ -13,3 +16,7 @@ for i in range(n):
 
 if not found:
     print("Element not found")
+
+end_time=time.time()
+execution_time=end_time-start_time
+print(f"Time taken to find element: {execution_time}")
