f37909a360652960d52d8b29da465c760ab9dda9
harsha <harshaa.bsc23@rvu.edu.in>
2025-03-08 21:46:48 +0530

    Bubble sort algo with time complexity

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,12 @@
+import time
 n=int(input("Enter the number of elements: "))
 print("Enter array elements one by one: ")
 arr=[int(input()) for _ in range(n)]
 
 swaps=0
 comparision=0
+
+start_time=time.time()
 
 for i in range(n-1):
     for j in range(n-1-i):
@@ -16,3 +19,8 @@ for i in range(n-1):
 print("sorted array using bubble sort: ",arr)
 print(f"Number of comparision: {comparision}")
 print(f"Number of swaps: {swaps}")
+
+end_time=time.time()
+execution_time=end_time-start_time
+
+print(f"time taken to sort using bubble sort: {execution_time}")
