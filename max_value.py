e087d035f8163ee63b417dae55e638c91bc44e8d
harsha <harshaa.bsc23@rvu.edu.in>
2025-03-18 21:57:18 +0530

    time complexity to max value

--- max_value.py
+++ max_value.py
@@ -1,3 +1,4 @@
+import time
 def find_Max(arr):
     max_value=arr[0]
     for i in range(1, len(arr)):
@@ -6,5 +7,10 @@ def find_Max(arr):
     return max_value
 
 arr=[int(input("enter the elements: ")) for _ in range(int(input("enter the number of elements: ")))]
+start_time=time.time()
 max_element=find_Max(arr)
+end_time=time.time()
+
 print("Maximum element in array: ", max_element)
+execution_time=end_time-start_time
+print("Time taken to find max element: ",execution_time)
